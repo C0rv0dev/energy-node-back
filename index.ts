51f00f0601ac8482f6e483e4106a0459c2ab8a6a
Ворон <lucascoutojp@gmail.com>
2024-04-04 15:04:03 +0900

    added config for better env management;

--- index.ts
+++ index.ts
@@ -4,25 +4,27 @@ if (process.env.NODE_ENV != 'production') require('dotenv').config();
 // Dependencies
 import cors from 'cors';
 import express from 'express';
+import config from './app/config/config';
 import connectToDatabase from './app/config/connectToDatabase';
 import notesController from './app/http/controllers/notesController';
 
 // Init
 const app = express();
-const PORT = process.env.PORT || 10;
+const PORT = config.port;
+const baseUrl = config.api_base_url;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // Database
 connectToDatabase();
 
 // Routing 
-app.get('/api/notes', notesController.fetchNotes);
-app.get('/api/notes/:id', notesController.getNote);
-app.post('/api/notes', notesController.createNote);
-app.put('/api/notes/:id', notesController.updateNote);
-app.delete('/api/notes/:id', notesController.deleteNote);
+app.get(`${baseUrl}/notes`, notesController.fetchNotes);
+app.get(`${baseUrl}/notes/:id`, notesController.getNote);
+app.post(`${baseUrl}/notes`, notesController.createNote);
+app.put(`${baseUrl}/notes/:id`, notesController.updateNote);
+app.delete(`${baseUrl}/notes/:id`, notesController.deleteNote);
 
 // Start server
 app.listen(PORT);
