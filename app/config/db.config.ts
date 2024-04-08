57f18e9f5db80760c3999cbae4ef2e80054fb55c
Ворон <lucascoutojp@gmail.com>
2024-04-09 04:38:16 +0900

    added new string format

--- app/config/db.config.ts
+++ app/config/db.config.ts
@@ -1,8 +1,10 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const DB_HOST = process.env.DB_HOST || 'energy-node_mongo_1';
+const DB_HOST = process.env.DB_HOST || 'mongodb';
+const APP_NAME = process.env.APP_NAME || 'react-app';
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASS = process.env.DB_PASS || 'root';
+const DB_PASS = process.env.DB_PASSWORD || 'root';
 
-const mongoUri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}`;
+const mongoUri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/?retryWrites=true&w=majority&appName=${APP_NAME}`;
+
 export default mongoUri;
